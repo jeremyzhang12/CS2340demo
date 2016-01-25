34c092fffed0e010f18f6a43ea676b7277b98f54
Jingtian Zhang <Jingtian@lawn-128-61-47-113.lawn.gatech.edu>
2016-01-25 13:37:14 -0500

    Initial commit

---
+++ app/src/androidTest/java/edu/jzhang729gatech/cs2340demo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.jzhang729gatech.cs2340demo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
